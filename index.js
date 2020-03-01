4d41ccc4907538325b978fe4434870f2d1387ecc
Anderson Nicacio <43139753+anicacio@users.noreply.github.com>
2020-03-01 00:13:23 -0300

    Dados e Nodemon

---
+++ index.js
@@ -0,0 +1,37 @@
+const PETSHOP = "Petshop DH";
+
+let pets = [{
+    nome: "Fred",
+    raca: "vira-lata",
+    idade: 17,
+    genero: "M",
+    servicos: [],
+    tipo: "gato",
+    vacinado: false,
+},{
+    nome: "Capivara",
+    raca: "vira-lata",
+    idade: 12,
+    genero: "M",
+    servicos: ["banho", "tosa", "vacinação"],
+    tipo: "gato",
+    vacinado: true,
+},{
+    nome: "Tripé",
+    raca: "angora",
+    idade: 15,
+    genero: "F",
+    servicos: [],
+    tipo: "gato",
+    vacinado: false,
+},{
+    nome: "Sóopó",
+    raca: "siames",
+    idade: 99,
+    genero: "F",
+    servicos: ["banho", "tosa", "vacinação"],
+    tipo: "gato",
+    vacinado: false,
+}]
+
+
